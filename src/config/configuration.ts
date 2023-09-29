274135bc94158cf7bfa1f8c8ac5f8b3a8685d611
Max Vresch <max.vresch@gmail.com>
2023-09-29 10:45:08 +0700

    add env HOST

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,4 +1,5 @@
 export default () => ({
+  host: process.env.HOST,
   port: parseInt(process.env.PORT, 10),
   db: process.env.DATABASE_URL,
 });
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const host = configService.get('HOST');
   const port = configService.get('PORT');
   await app.listen(port);
-  Logger.log(`Server is bootstrapped on port: ${port}`);
+  Logger.log(`Server is bootstrapped: http://${host}:${port}`);
 }
 
 bootstrap();
