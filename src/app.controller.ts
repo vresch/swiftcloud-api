c5a5947a457f89fb2c6f0f976a1071a31130f92b
Max Vresch <max.vresch@gmail.com>
2023-10-04 11:52:04 +0700

    rm REST API controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { SongService } from './song.service';
-import { SongStat as SongStatModel } from '@prisma/client';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly songService: SongService) {}
-
-  @Get()
-  getAllSongStat(): Promise<SongStatModel[]> {
-    return this.songService.allSongStat();
-  }
-
-  @Get(':id')
-  getSongStat(@Param('id') id: string): Promise<SongStatModel> {
-    return this.songService.songStat({ id: Number(id) });
-  }
+  constructor() {}
 }
